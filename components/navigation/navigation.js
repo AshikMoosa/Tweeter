513232c869fc95655c09eb20b9a05db5f7f6e400
ashikmoosa <ashikmoosa96@gmail.com>
2023-11-30 15:23:51 +0530

    Added navigation styles and event to selected navItem

---
+++ components/navigation/navigation.js
@@ -0,0 +1,13 @@
+const navContainer = document.querySelector(".navigation");
+const navList = navContainer.querySelectorAll("li");
+const navItem = navContainer.querySelectorAll("a");
+
+navItem.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    navList.forEach((listItem) => {
+      listItem.removeAttribute("selected");
+    });
+    // setting selected attribute to respective <li> element
+    e.target.parentElement.setAttribute("selected", true);
+  });
+});
