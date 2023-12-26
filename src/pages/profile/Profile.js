964d5a3380105b66aad2c8098002f257b1e20e9f
ashikmoosa <ashikmoosa96@gmail.com>
2023-12-26 19:43:09 +0530

    Added profile page

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,7 +1,35 @@
 const renderProfilePage = `
+
 <section class="profile-page">
   <section class="main">
-    <h1>This is profile page</h1>
+    <div class="cover-image">
+      <img src="/src/assets/profile-cover.jpg" alt="cover-img">
+    </div>
+    <div class="user-profile">
+      <div class="user-image">
+        <img src="/src/assets/profile1.jpg" alt="user-image">
+      </div>
+      <div class="user-info">
+        <div class="user-details">
+          <div class="details-container">
+            <h2 class="title">Cristina Neal</h2>
+            <span class="following-count"><b>2569</b> Following</span>
+            <span class="follower-count"><b>10.8K</b> Followers</span>
+          </div>
+
+
+          <div class="user-description">
+            Photographer & Filmmaker based in Copenhagen, Denmark ✵ 🇩🇰
+          </div>
+        </div>
+        <button type="button" class="follow-button"><i
+            class="fa-solid fa-user-plus fa-flip-horizontal"></i>Follow</button>
+
+      </div>
+    </div>
+  </section>
+  <section class="bookmarks" style="border: 1px solid black;">
+    <span>this is bookmark section</span>
   </section>
 </section>
 `;
